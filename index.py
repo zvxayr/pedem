6cfee0d32952c8a29071836d5706ea9a28e2083c
zvxayr <carlo4noles@gmail.com>
2023-01-20 17:59:48 +0800

    -refactor changed variable names

--- index.py
+++ index.py
@@ -8,19 +8,19 @@ from foot_measurement import convert_px_to_cm, get_foot_px
 def process_image(file_path, bounds):
     img = cv2.imread(file_path, 0)
     img = slice_image(img, **bounds)
-    img = detect_edges(img)
-    return img
+    edge_img = detect_edges(img)
+    return edge_img
 
 
-def get_foot_size(img):
-    px = get_foot_px(img)
+def get_foot_size(edge_img):
+    px = get_foot_px(edge_img)
     cm = convert_px_to_cm(px)
     return cm
 
 
 if __name__ == "__main__":
     file_path = "Samples/S02 24.5.jpg"
-    img = process_image(file_path, config.image_bounds)
-    cm = get_foot_size(img)
+    edge_img = process_image(file_path, config.image_bounds)
+    cm = get_foot_size(edge_img)
 
     print(cm)
