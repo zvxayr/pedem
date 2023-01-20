21238d65d7bf86ac34005d953b4f3aa8ebac2c56
zvxayr <carlo4noles@gmail.com>
2023-01-20 17:58:02 +0800

    -refactor extracted functions from main

--- index.py
+++ index.py
@@ -4,12 +4,23 @@ import config
 from canny_edge_detection import detect_edges, slice_image
 from foot_measurement import convert_px_to_cm, get_foot_px
 
-if __name__ == "__main__":
-    file_path = "Samples/S02 24.5.jpg"
+
+def process_image(file_path, bounds):
     img = cv2.imread(file_path, 0)
-    img = slice_image(img, **config.image_bounds)
+    img = slice_image(img, **bounds)
     img = detect_edges(img)
+    return img
+
+
+def get_foot_size(img):
     px = get_foot_px(img)
     cm = convert_px_to_cm(px)
+    return cm
+
+
+if __name__ == "__main__":
+    file_path = "Samples/S02 24.5.jpg"
+    img = process_image(file_path, config.image_bounds)
+    cm = get_foot_size(img)
 
     print(cm)
