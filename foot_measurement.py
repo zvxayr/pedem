9ab7600563e48ca57777fd883a545d0f11815261
zvxayr <carlo4noles@gmail.com>
2023-01-20 18:05:52 +0800

    -refactor renamed variables

--- foot_measurement.py
+++ foot_measurement.py
@@ -7,26 +7,26 @@ from skimage.morphology import disk
 import config
 
 
-def process_edge_image(edge_img, kernel):
-    edge_img = dilate(edge_img, kernel, iterations=1)
-    edge_img = erode(edge_img, kernel, iterations=1)
-    return edge_img
+def process_edge_image(edge_image, kernel):
+    edge_image = dilate(edge_image, kernel, iterations=1)
+    edge_image = erode(edge_image, kernel, iterations=1)
+    return edge_image
 
 
-def find_largest_closed_boundary_side_px(edge_img):
-    contours, _ = findContours(edge_img, 1, 2)
+def find_largest_closed_boundary_side_px(edge_image):
+    contours, _ = findContours(edge_image, 1, 2)
 
-    largest = 0
-    for cnt in contours:
-        (_, _), (w, h), _ = minAreaRect(cnt)
-        largest = max(largest, w, h)
+    largest_side = 0
+    for contour in contours:
+        (_, _), (w, h), _ = minAreaRect(contour)
+        largest_side = max(largest_side, w, h)
 
-    return largest
+    return largest_side
 
 
-def get_foot_px(edge_img):
-    processed_edge_img = process_edge_image(edge_img, disk(4))
-    foot_px = find_largest_closed_boundary_side_px(processed_edge_img)
+def get_foot_px(edge_image):
+    processed_edge_image = process_edge_image(edge_image, disk(4))
+    foot_px = find_largest_closed_boundary_side_px(processed_edge_image)
     return foot_px
 
 
@@ -36,6 +36,6 @@ def load_model(model_source):
         return pickle.load(f)
 
 
-def convert_px_to_cm(x, model_source=config.model_file):
+def convert_px_to_cm(px, model_source=config.model_file):
     reg = load_model(model_source)
-    return reg.predict([[x]])[0]
+    return reg.predict([[px]])[0]
